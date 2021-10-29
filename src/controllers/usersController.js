b5c7c4fa76d320623efbe486650c15fff021ac7b
UrielSosa <sosa.uriel1999@gmail.com>
2021-10-28 23:35:37 -0300

    agregamos hash a contraseñas del register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,11 @@
 const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+
+let users = fs.readFileSync(path.resolve(__dirname, '../db/users.json'), { encoding: 'utf-8'});
+users = JSON.parse(users);
+
 
 let controller = {
     register: (req,res)=>{
@@ -7,7 +13,25 @@ let controller = {
     },
     registered: function (req, res) {
         const errors = validationResult(req);
-        return res.send(errors.mapped());
+
+        if (!errors.isEmpty()) {
+            res.render('users/register', errors.mapped());
+        }else {
+            const user = {
+                ...req.body,
+                password: bcrypt.hashSync(req.body.password, 12),
+                image: req.file.filename
+            }
+            delete user.repassword;
+            
+            users.push(user);
+
+            let jsonDeUsuarios = JSON.stringify(users, null, 4);
+            fs.writeFileSync(path.resolve(__dirname, '../db/users.json'), jsonDeUsuarios);
+            return res.redirect('/users/login');
+        }
+        
+
     },
     login (req,res) {
         res.render('users/login');
